2134b553f2d54481db42046bc6fb359a765a122b
Jamoben <146975543+Jamoben@users.noreply.github.com>
2023-11-20 13:11:03 +0100

    Update test_DyV.cpp busqueda binaria invertida

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -4,13 +4,21 @@
 
 int main(){
 
-        vector<char> c = {'a','b','c','d','e','f','g','i'};
+        vector<char> c = {'a','b','c','d','e','f','g','h'};
         vector<int> v = {1,2,3,4,5,6,7,8,9};
         vector<double> d = {1.0,2.0,3.0,4.0,5.0};
 
         char prim = BusquedaBinaria("b", c, 0, 7);
         int  sec = BusquedaBinaria(7, v, 0, 8);
-        double ter = BusquedaBinaria(3.0, f, 0, 4);
+        double ter = BusquedaBinaria(3.0, d, 0, 4);
 
-        std:: cout << "%c %d %f" << prim, sec, ter << std::endl;       
+        std:: cout << "%c %d %f" << prim, sec, ter << std::endl;   
+
+        vector<char> ic = {'h','g','f','e','d','c','b','a',};
+        vector<int> iv = {9,8,7,6,5,4,3,2,1};
+        vector<double> id = {5.0,4.0,3.0,2.0,1.0};
+
+        char iprim = BusquedaBinaria("b", ic, 0, 7);
+        int  isec = BusquedaBinaria(7, iv, 0, 8);
+        double iter = BusquedaBinaria(3.0, id, 0, 4);
 }
