c603487de3bff4682a6c7c98602b15d6960db91f
Jamoben <146975543+Jamoben@users.noreply.github.com>
2023-11-20 13:07:25 +0100

    Update busqueda binaria invertida

--- DyV.h
+++ DyV.h
@@ -1,7 +1,8 @@
 #include <vector>
+#include <iostream>
 
 template <typename T>
-T BusquedaBinaria(T x, std::vector<T>& v, int ini, int fin){
+int BusquedaBinaria(T x, std::vector<T>& v, int ini, int fin){
         if(ini > fin){
                 throw ("No se encuentra en el array");
         }
@@ -17,5 +18,23 @@ T BusquedaBinaria(T x, std::vector<T>& v, int ini, int fin){
         }
         else{
                 return BusquedaBinaria(x, v, medio +1, fin);
+        }
+}
+
+int BusquedaBinaria_INV(T x, vector<T>& v,int ini, int fin){
+        if(ini > fin){
+                throw ("No se encuentra en el array");
+        }
+        
+        int medio = (ini + fin) / 2;
+        
+        if(v[medio] == x){
+                return medio;
+        }
+        
+        else if(v[medio] < x){
+                return BusquedaBinaria_INV(x, v, ini,medio -1);
+        }else{
+		return BusquedaBinaria_INV(x, v, medio +1, fin);
         }
 }
